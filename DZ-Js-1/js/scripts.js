f18a8b3ac58dff44fe7dcc56ca5d6b5502f52a2e
Podoselov <alexpodoselov1@gmail.com>
2021-07-30 01:19:36 +0300

    add dz-js-1

--- DZ-Js-1/js/scripts.js
+++ DZ-Js-1/js/scripts.js
@@ -1,1 +1,30 @@
 'use strict';
+let userName = '';
+let userAge = '';
+do {
+  userName = prompt('Enter your name', `${userName}`);
+  userAge = prompt('How old are you', `${userAge}`);
+} while (
+  userName === null ||
+  userName.trim() === '' ||
+  userAge === null ||
+  typeof +userAge !== 'number' ||
+  isNaN(+userAge) ||
+  userAge.trim() === ''
+);
+
+function authorization(name, age) {
+  let errorMessage = 'You are not allowed to visit this website';
+  let allowed = `Welcome,  ${name} !`;
+  if (age < 18) {
+    alert(`${errorMessage}`);
+  } else if (age > 22) {
+    alert(`${allowed}`);
+  } else {
+    let continuationQuestion = confirm('Are you sure you want to continue?')
+      ? alert(`${allowed}`)
+      : alert(`${errorMessage}`);
+  }
+}
+
+authorization(userName, userAge);
