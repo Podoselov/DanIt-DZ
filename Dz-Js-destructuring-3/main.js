010027a3896b980ccbf0259dc0f212dfc83e47e7
Podoselov <alexpodoselov1@gmail.com>
2021-10-26 17:54:20 +0300

    add change

--- Dz-Js-destructuring-3/main.js
+++ Dz-Js-destructuring-3/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//Task1
+// Task1
 
 const clients1 = [
   'Гилберт',
@@ -13,162 +13,152 @@ const clients1 = [
 ];
 const clients2 = ['Пирс', 'Зальцман', 'Сальваторе', 'Майклсон'];
 
-// function newClients([...arr]) {
-//   arr.filter(function (el) {
-//     return el;
-//   });
-// }
-
-const [...clients3] = [...clients1, ...clients2];
-clients3.filter(function (item, pos) {
-  return item === pos;
-});
+const clients3 = [...new Set([...clients1, ...clients2])];
 
 console.log(clients3);
-// //Task2
 
-// const characters = [
-//   {
-//     name: 'Елена',
-//     lastName: 'Гилберт',
-//     age: 17,
-//     gender: 'woman',
-//     status: 'human',
-//   },
-//   {
-//     name: 'Кэролайн',
-//     lastName: 'Форбс',
-//     age: 17,
-//     gender: 'woman',
-//     status: 'human',
-//   },
-//   {
-//     name: 'Аларик',
-//     lastName: 'Зальцман',
-//     age: 31,
-//     gender: 'man',
-//     status: 'human',
-//   },
-//   {
-//     name: 'Дэймон',
-//     lastName: 'Сальваторе',
-//     age: 156,
-//     gender: 'man',
-//     status: 'vampire',
-//   },
-//   {
-//     name: 'Ребекка',
-//     lastName: 'Майклсон',
-//     age: 1089,
-//     gender: 'woman',
-//     status: 'vempire',
-//   },
-//   {
-//     name: 'Клаус',
-//     lastName: 'Майклсон',
-//     age: 1093,
-//     gender: 'man',
-//     status: 'vampire',
-//   },
-// ];
+// Task2
 
-// let [...charactersShortInfo] = characters.map((el) => {
-//   return { name: el.name, lastName: el.lastName, age: el.age };
-// });
+const characters = [
+  {
+    name: 'Елена',
+    lastName: 'Гилберт',
+    age: 17,
+    gender: 'woman',
+    status: 'human',
+  },
+  {
+    name: 'Кэролайн',
+    lastName: 'Форбс',
+    age: 17,
+    gender: 'woman',
+    status: 'human',
+  },
+  {
+    name: 'Аларик',
+    lastName: 'Зальцман',
+    age: 31,
+    gender: 'man',
+    status: 'human',
+  },
+  {
+    name: 'Дэймон',
+    lastName: 'Сальваторе',
+    age: 156,
+    gender: 'man',
+    status: 'vampire',
+  },
+  {
+    name: 'Ребекка',
+    lastName: 'Майклсон',
+    age: 1089,
+    gender: 'woman',
+    status: 'vempire',
+  },
+  {
+    name: 'Клаус',
+    lastName: 'Майклсон',
+    age: 1093,
+    gender: 'man',
+    status: 'vampire',
+  },
+];
 
-// console.log(charactersShortInfo);
+let [...charactersShortInfo] = characters.map((el) => {
+  return { name: el.name, lastName: el.lastName, age: el.age };
+});
 
-//Task3
+console.log(charactersShortInfo);
 
-// const user1 = {
-//   name: 'John',
-//   years: 30,
-// };
+// Task3
 
-// const { name: name, years: years, isAdmin = false } = user1;
+const user1 = {
+  name: 'John',
+  years: 30,
+};
 
-// console.log(name, years, isAdmin);
+const { name: name, years: years, isAdmin = false } = user1;
 
-//Task 4
+console.log(name, years, isAdmin);
 
-// const satoshi2020 = {
-//   name: 'Nick',
-//   surname: 'Sabo',
-//   age: 51,
-//   country: 'Japan',
-//   birth: '1979-08-21',
-//   location: {
-//     lat: 38.869422,
-//     lng: 139.876632,
-//   },
-// };
+// Task 4
 
-// const satoshi2019 = {
-//   name: 'Dorian',
-//   surname: 'Nakamoto',
-//   age: 44,
-//   hidden: true,
-//   country: 'USA',
-//   wallet: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
-//   browser: 'Chrome',
-// };
+const satoshi2020 = {
+  name: 'Nick',
+  surname: 'Sabo',
+  age: 51,
+  country: 'Japan',
+  birth: '1979-08-21',
+  location: {
+    lat: 38.869422,
+    lng: 139.876632,
+  },
+};
 
-// const satoshi2018 = {
-//   name: 'Satoshi',
-//   surname: 'Nakamoto',
-//   technology: 'Bitcoin',
-//   country: 'Japan',
-//   browser: 'Tor',
-//   birth: '1975-04-05',
-// };
+const satoshi2019 = {
+  name: 'Dorian',
+  surname: 'Nakamoto',
+  age: 44,
+  hidden: true,
+  country: 'USA',
+  wallet: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
+  browser: 'Chrome',
+};
 
-//Task5
+const satoshi2018 = {
+  name: 'Satoshi',
+  surname: 'Nakamoto',
+  technology: 'Bitcoin',
+  country: 'Japan',
+  browser: 'Tor',
+  birth: '1975-04-05',
+};
 
-// const books = [
-//   {
-//     name: 'Harry Potter',
-//     author: 'J.K. Rowling',
-//   },
-//   {
-//     name: 'Lord of the rings',
-//     author: 'J.R.R. Tolkien',
-//   },
-//   {
-//     name: 'The witcher',
-//     author: 'Andrzej Sapkowski',
-//   },
-// ];
+const fullProfile = { ...satoshi2018, ...satoshi2019, ...satoshi2020 };
+console.log(fullProfile);
 
-// const bookToAdd = {
-//   name: 'Game of thrones',
-//   author: 'George R. R. Martin',
-// };
+// Task5
 
-// const [...book2] = [...books, { ...bookToAdd }];
+const books = [
+  {
+    name: 'Harry Potter',
+    author: 'J.K. Rowling',
+  },
+  {
+    name: 'Lord of the rings',
+    author: 'J.R.R. Tolkien',
+  },
+  {
+    name: 'The witcher',
+    author: 'Andrzej Sapkowski',
+  },
+];
 
-// console.log(book2);
+const bookToAdd = {
+  name: 'Game of thrones',
+  author: 'George R. R. Martin',
+};
 
-//Task6
+const [...book2] = [...books, { ...bookToAdd }];
 
-// const employee = {
-//   name: 'Vitalii',
-//   surname: 'Klichko',
-// };
+console.log(book2);
 
-// function addArg({ ...newObj }) {
-//   newObj.age = 22;
-//   newObj.salary = 22222;
-//   return newObj;
-// }
+// Task6
 
-// console.log(addArg(employee));
+const employee = {
+  name: 'Vitalii',
+  surname: 'Klichko',
+};
 
-//Task7
+const newObj = { ...employee, age: 22, salary: 22222 };
+console.log(newObj);
 
-// const array = ['value', () => 'showValue'];
+// Task7
 
-// const [value] = array;
-// const [, showValue] = array;
+const array = ['value', () => 'showValue'];
 
-// console.log(value);
-// console.log(showValue());
+const [value] = array;
+const [, showValue] = array;
+
+console.log(value);
+console.log(showValue());
